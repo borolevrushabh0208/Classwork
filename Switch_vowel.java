79e20db7d7a2a5405811847361783ec9656f5435
borolevrushabh0208 <106954015+borolevrushabh0208@users.noreply.github.com>
2022-06-23 14:02:17 +0530

    Add files via upload

---
+++ Switch_vowel.java
@@ -0,0 +1,80 @@
+//Enter a character and check it is vowel or consonant Through switch and case.
+import java.util.*;
+class Switch_vowel
+{
+	public static void main(String[]args)
+	{
+	 Scanner obj=new Scanner(System.in);
+	 System.out.println("\t\tenter a charecter: ");
+	 char ch=obj.next().charAt(0);
+
+	 switch(ch)
+	 {
+	 case'a':
+	 	{
+	 		System.out.println("\t\tit is vowel");
+	 		break;
+	 	}
+
+	 case'e':
+	 	{
+	 		System.out.println("\t\tit is vowel");
+	 		break;
+	 	}
+
+	 case'i':
+	 	{
+	 		System.out.println("\t\tit is vowel");
+	 		break;
+	 	}
+
+	 case'o':
+	 	{
+	 		System.out.println("\t\tit is vowel");
+	 		break;
+	 	}
+
+	 case'u':
+	 	{
+	 		System.out.println("\t\tit is vowel");
+	 		break;
+	 	}
+
+	 case'A':
+	 	{
+	 		System.out.println("\t\tit is vowel");
+	 		break;
+	 	}
+
+	case'E':
+	 	{
+	 		System.out.println("\t\tit is vowel");
+	 		break;
+	 	}
+
+	case'I':
+	 	{
+	 		System.out.println("\t\tit is vowel");
+	 		break;
+	 	}
+
+	case'O':
+	 	{
+	 		System.out.println("\t\tit is vowel");
+	 		break;
+	 	}
+
+	case'U':
+	 	{
+	 		System.out.println("\t\tit is vowel");
+	 		break;
+	 	}
+
+	 default:
+	 		System.out.println("\t\tit is consonant :");
+
+	 }//switch
+
+
+	}//main
+}
